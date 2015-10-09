c9eee0bf4e3a3fa9123d685a38b432e63eabe01d
Yoel Monzon <yoelfme@hotmail.com>
2015-10-09 12:05:10 -0600

    [add] lesson 1 and lesson 2 :grin:

---
+++ lesson2/lesson2.js
@@ -0,0 +1,19 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8000)
+});
+
+server.route({
+    path: '/{name}',
+    method: 'GET',
+    handler: function (request, reply) {
+        reply('Hello ' + encodeURIComponent(request.params.name))
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+})
