e839f5ae21d09c1434417dcda2b7882442c5d21f
Yoel Monzon <yoelfme@hotmail.com>
2015-10-09 15:28:36 -0600

    [add] finished lesson 6 :flushed:

---
+++ lesson6/index.js
@@ -0,0 +1,28 @@
+var Hapi = require('hapi');
+var H2o2 = require('h2o2');
+
+var server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(H2o2, function (err) {
+    if (err) throw err;
+});
+
+server.route({
+    method: 'GET',
+    path: '/proxy',
+    handler: {
+        proxy: {
+            host: '127.0.0.1',
+            port: 65535
+        }
+    }
+})
+
+server.start(function () {
+    console.log('Server at listening at: ' + server.info.uri);
+});
