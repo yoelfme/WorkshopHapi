e7105e79610e4d67fa7984d20170e11d0f16412e
Yoel Monzon <yoelfme@hotmail.com>
2015-10-10 22:40:51 -0600

    [add] finished lesson 12 :satisfied:

---
+++ lesson12/index.js
@@ -0,0 +1,55 @@
+var Hapi = require('hapi');
+var Boom = require('boom');
+
+var server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.state('session', {
+    path: '/',
+    encoding: 'base64json',
+    ttl: 10,
+    domain: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/set-cookie',
+    handler: function(request, reply) {
+        reply({
+            message: 'success',
+        }).state('session', { key: 'makemehapi' })
+    },
+    config: {
+        state: {
+            parse: true,
+            failAction: 'log'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/check-cookie',
+    handler: function (request, reply) {
+        var session = request.state.session;
+        var response = null;
+
+        if (session) {
+            response = {
+                user: 'hapi'
+            };
+        } else {
+            response = Boom.unauthorized('Missing authentication');
+        }
+
+        reply(response);
+    }
+})
+
+server.start(function () {
+    console.log('Server listening at: ' + server.info.uri);
+});
